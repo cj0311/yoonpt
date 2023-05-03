08f96a82f7320d6e611d8cfcda7a2b66e9f00077
chagnjin <quritis@naver.com>
2023-05-03 18:23:42 +0900

    refactor yoonpagoAdmin.js

--- androidjs/yoonpagoAdmin.js
+++ androidjs/yoonpagoAdmin.js
@@ -1,4 +1,5 @@
 const scriptName = "yoonpagoAdmin";
+
 /**
  * (string) room
  * (string) sender
@@ -8,85 +9,55 @@ const scriptName = "yoonpagoAdmin";
  * (string) imageDB.getProfileBase64()
  * (string) packageName
  */
+
+
 importPackage(java.io);
 importPackage(java.lang);
 
+function handleCommand(room, msg, sender, isGroupChat, replier) {
+    if (sender !== "jinn" || room !== "윤피티admin") return false;
+
+    const command = msg.split(' ')[0];
+    const scriptname = msg.slice(command.length + 1);
+
+    if (command === "/off" || command === "/on" || command === "/reload") {
+        Log.debug(command);
+        Log.debug(scriptname);
+
+        if (command === "/off") {
+            Api.off(scriptname);
+        } else if (command === "/on") {
+            Api.on(scriptname);
+        } else {
+            Api.reload(scriptname);
+        }
+        replier.reply(scriptname + " is " + command.slice(1));
+        return true;
+    }
+    return false;
+}
+
 function responseFix(room, msg, sender, isGroupChat, replier, imageDB, packageName) {
     try {
-      if (sender == "jinn" && room == "윤피티admin") {
-        if (msg.startsWith("/off ")) {
-          var commandLength = "/off ".length;
-          var scriptname = msg.substr(commandLength);
+        if (handleCommand(room, msg, sender, isGroupChat, replier)) return;
 
-          Log.debug("/off ");
-          Log.debug(scriptname);
-          replier.reply(scriptname + " is off");
-          Api.off(scriptname);
-          return;
+        if (sender === "jinn" && room === "윤피티admin") {
+            room = "윤피티Debug";
+            retunrMsg = "msg [" + sender + "] : " + msg;
 
+            Api.replyRoom(room, retunrMsg, hideToast = false);
         }
-
-        if (msg.startsWith("/on ")) {
-          var commandLength = "/on ".length;
-          var scriptname = msg.substr(commandLength);
-
-          Log.debug("/on ");
-          Log.debug(scriptname);
-          replier.reply(scriptname + " is on");
-          Api.on(scriptname);
-          return;
-        }
-
-
-        if (msg.startsWith("/reload ")) {
-          var commandLength = "/reload ".length;
-          var scriptname = msg.substr(commandLength);
-
-          Log.debug("/reload ");
-          Log.debug(scriptname);
-          replier.reply(scriptname + " is reload");
-          Api.reload(scriptname);
-          return;
-        }
-
-        // var currentDir = new File('.').getCanonicalPath();
-        // Log.debug(currentDir);
-        // replier.reply(currentDir);
-
-
-        // var runtime = Runtime.getRuntime();
-        // var process = runtime.exec('ls');
-
-        // var input = new BufferedReader(new InputStreamReader(process.getInputStream()));
-        // var line;
-        // replier.reply(11);
-        // while ((line = input.readLine()) !== null) {
-        //   replier.reply(line);
-        // }
-
-        // replier.reply(22);
-
-        room = "윤피티Debug";
-        retunrMsg = "msg [" + sender + "] : " + msg;
-
-        Api.replyRoom(room, retunrMsg, hideToast = false);
-      }
-
-
-    }
-    catch (e) {
-      room = "윤피티admin";
-      Api.replyRoom(room, e, hideToast = false)
-
+    } catch (e) {
+        room = "윤피티admin";
+        Api.replyRoom(room, e, hideToast = false);
     }
 }
 
-//아래 4개의 메소드는 액티비티 화면을 수정할때 사용됩니다.
 function onCreate(savedInstanceState, activity) {
-  var textView = new android.widget.TextView(activity);
-  textView.setText("Hello, World!");
-  textView.setTextColor(android.graphics.Color.DKGRAY);
-  activity.setContentView(textView);
+    var textView = new android.widget.TextView(activity);
+    textView.setText("Hello, World!");
+    textView.setTextColor(android.graphics.Color.DKGRAY);
+    activity.setContentView(textView);
 }
 
 function onStart(activity) { }
@@ -98,30 +69,30 @@ function onPause(activity) { }
 function onStop(activity) { }
 
 function onNotificationPosted(sbn, sm) {
-  var packageName = sbn.getPackageName();
-  if (!packageName.startsWith("com.kakao.tal")) return;
-  var actions = sbn.getNotification().actions;
-  if (actions == null) return;
-  var userId = sbn.getUser().hashCode();
-  for (var n = 0; n < actions.length; n++) {
-    var action = actions[n];
-    if (action.getRemoteInputs() == null) continue;
-    var bundle = sbn.getNotification().extras;
+    var packageName = sbn.getPackageName();
+    if (!packageName.startsWith("com.kakao.tal")) return;
+    var actions = sbn.getNotification().actions;
+    if (actions == null) return;
+    var userId = sbn.getUser().hashCode();
+    for (var n = 0; n < actions.length; n++) {
+        var action = actions[n];
+        if (action.getRemoteInputs() == null) continue;
+        var bundle = sbn.getNotification().extras;
 
-    var msg = bundle.get("android.text").toString();
-    var sender = bundle.getString("android.title");
-    var room = bundle.getString("android.subText");
-    if (room == null) room = bundle.getString("android.summaryText");
-    var isGroupChat = room != null;
-    if (room == null) room = sender;
-    var replier = new com.xfl.msgbot.script.api.legacy.SessionCacheReplier(packageName, action, room, false, "");
-    var icon = bundle.getParcelableArray("android.messages")[0].get("sender_person").getIcon().getBitmap();
-    var image = bundle.getBundle("android.wearable.EXTENSIONS");
-    if (image != null) image = image.getParcelable("background");
-    var imageDB = new com.xfl.msgbot.script.api.legacy.ImageDB(icon, image);
-    com.xfl.msgbot.application.service.NotificationListener.Companion.setSession(packageName, room, action);
-    if (this.hasOwnProperty("responseFix")) {
-      responseFix(room, msg, sender, isGroupChat, replier, imageDB, packageName, userId != 0);
-    }
-  }
+        var msg = bundle.get("android.text").toString();
+        var sender = bundle.getString("android.title");
+        var room = bundle.getString("android.subText");
+        if (room == null) room = bundle.getString("android.summaryText");
+        var isGroupChat = room != null;
+        if (room == null) room = sender;
+        var replier = new com.xfl.msgbot.script.api.legacy.SessionCacheReplier(packageName, action, room, false, "");
+        var icon = bundle.getParcelableArray("android.messages")[0].get("sender_person").getIcon().getBitmap();
+        var image = bundle.getBundle("android.wearable.EXTENSIONS");
+        if (image != null) image = image.getParcelable("background");
+        var imageDB = new com.xfl.msgbot.script.api.legacy.ImageDB(icon, image);
+        com.xfl.msgbot.application.service.NotificationListener.Companion.setSession(packageName, room, action);
+        if (this.hasOwnProperty("responseFix")) {
+            responseFix(room, msg, sender, isGroupChat, replier, imageDB, packageName, userId != 0);
+            }
+        }
 }
