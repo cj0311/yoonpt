94ff5d42897e06fe607a73a243475238ac63677a
chagnjin <quritis@naver.com>
2023-05-03 18:17:56 +0900

    refactor yoonpagoF js

--- androidjs/yoonpagoF.js
+++ androidjs/yoonpagoF.js
@@ -1,13 +1,8 @@
 const scriptName = "yoonpagoF";
-
-const BufferedReader = java.io.BufferedReader;
-const DataOutputStream = java.io.DataOutputStream;
-const InputStreamReader = java.io.InputStreamReader;
-const HttpURLConnection = java.net.HttpURLConnection;
-const URL = java.net.URL;
-const URLEncoder = java.net.URLEncoder;
 importPackage(java.net);
 importPackage(java.io);
+
+
 /**
  * (string) room
  * (string) sender
@@ -20,67 +15,53 @@ importPackage(java.io);
 
 
 function responseFix(room, msg, sender, isGroupChat, replier, imageDB, packageName) {
-
-    if(true){
-    try {
-        if(msg=="git clone")
-            replier.reply(httpGet("http://yoonpt.synology.me:8088/gitclone"));
-
-        if (msg.includes("헷") || msg.startsWith("피티 ") || msg.startsWith("피티야") || msg.startsWith("파고야") || msg.startsWith("윤파고") || msg.startsWith("윤피티")) {
-            let gptmsg = msg.replace("피티야", "").replace("파고야", "").replace("윤파고", "").replace("윤피티", "").replace("헷", "").replace("피티 ", "").trim();
-            if (gptmsg == "") {
+    if (true) {
+        try {
+            if (msg == "git clone") {
+                replier.reply(httpGet("http://yoonpt.synology.me:8088/gitclone"));
             }
-            else {
-                var encodedMsg = encodeSpecialChars(gptmsg);
-                var encodedRoom = encodeSpecialChars(room);
-                var encodedSender = encodeSpecialChars(sender);
-                var url = 'http://yoonpt.synology.me:8088/post_message/';
 
-                var data = {
-                    "room": encodedRoom,
-                    "msg": encodedMsg,
-                    "sender": encodedSender
-                };
-                var response = httpPost(url, data); // JSON 응답
-                // JSON 응답에서 필요한 정보 추출
-                var result = response.result;
-                var details = response.details;
-                var timestamp = response.timestamp;
-
-                // 응답 정보를 사용하여 메시지 작성
-                //var replyMessage = "Result: " + result + "\nDetails: " + details + "\nTimestamp: " + timestamp;
-                var replyMessage = details;
-
-                replier.reply(replyMessage);
+            if (isMessageRelevant(msg)) {
+                let gptmsg = processMessage(msg);
+                if (gptmsg != "") {
+                    let data = {
+                        "room": encodeSpecialChars(room),
+                        "msg": encodeSpecialChars(gptmsg),
+                        "sender": encodeSpecialChars(sender)
+                    };
+                    let response = httpPost('http://yoonpt.synology.me:8088/post_message/', data);
+                    let replyMessage = response.details;
+                    replier.reply(replyMessage);
+                }
             }
+        } catch (e) {
+            Api.replyRoom("윤피티Debug", e, hideToast = false)
         }
     }
-        catch (e) {
-            room = "윤피티Debug";
-            Api.replyRoom(room, e, hideToast = false)
-        }
-    }
-    
+}
+
+function isMessageRelevant(msg) {
+    return msg.includes("헷") || msg.startsWith("피티 ") || msg.startsWith("피티야") || msg.startsWith("파고야") || msg.startsWith("윤파고") || msg.startsWith("윤피티");
+}
+
+function processMessage(msg) {
+    return msg.replace("피티야", "").replace("파고야", "").replace("윤파고", "").replace("윤피티", "").replace("헷", "").replace("피티 ", "").trim();
 }
 
 function httpGet(url) {
-    var URL = java.net.URL;
-    var BufferedReader = java.io.BufferedReader;
-    var InputStreamReader = java.io.InputStreamReader;
-
-    var urlObj = new URL(url);
-    var connection = urlObj.openConnection();
+    let urlObj = new URL(url);
+    let connection = urlObj.openConnection();
     connection.setRequestMethod("GET");
     connection.connect();
 
-    var responseCode = connection.getResponseCode();
-    room = "윤피티Debug";
-    Api.replyRoom(room, responseCode, hideToast = false)
-    var inputStream = connection.getInputStream();
-    var reader = new BufferedReader(new InputStreamReader(inputStream));
+    let responseCode = connection.getResponseCode();
+    Api.replyRoom("윤피티Debug", responseCode, hideToast = false);
 
-    var response = "";
-    var line;
+    let inputStream = connection.getInputStream();
+    let reader = new BufferedReader(new InputStreamReader(inputStream));
+
+    let response = "";
+    let line;
     while ((line = reader.readLine()) != null) {
         response += line;
     }
@@ -90,34 +71,27 @@ function httpGet(url) {
 }
 
 
-
-
 function httpPost(url, data) {
     try {
-        var json = JSON.stringify(data);
-        //room = "윤피티Debug";
-        //Api.replyRoom(room, json, hideToast = false)
-
-        var urlObj = new URL(url);
-        var connection = urlObj.openConnection();
+        let json = JSON.stringify(data);
+        let urlObj = new URL(url);
+        let connection = urlObj.openConnection();
         connection.setDoOutput(true);
         connection.setRequestMethod("POST");
         connection.setRequestProperty("Content-Type", "application/json;charset=UTF-8");
 
-        var outputStream = new DataOutputStream(connection.getOutputStream());
+        let outputStream = new DataOutputStream(connection.getOutputStream());
         outputStream.writeBytes(json);
         outputStream.flush();
         outputStream.close();
 
-        var responseCode = connection.getResponseCode();
-        //room = "윤피티Debug";
-        //Api.replyRoom(room, responseCode, hideToast = false)
+        let responseCode = connection.getResponseCode();
 
         if (responseCode >= 200 && responseCode < 400) {
-            var inputStream = connection.getInputStream();
-            var reader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
-            var response = "";
-            var line = null;
+            let inputStream = connection.getInputStream();
+            let reader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
+            let response = "";
+            let line = null;
             while ((line = reader.readLine()) != null) {
                 response += line;
             }
@@ -127,9 +101,8 @@ function httpPost(url, data) {
             throw new Error('Error: ' + responseCode + ' ' + connection.getResponseMessage());
         }
     } catch (e) {
-        room = "윤피티Debug";
-        Api.replyRoom(room, 'Error in httpPost: ' + e.message, hideToast = false)
-        return ;
+        Api.replyRoom("윤피티Debug", 'Error in httpPost: ' + e.message, hideToast = false);
+        return;
     }
 }
 
@@ -139,109 +112,91 @@ function encodeSpecialChars(str) {
 
 function decodeSpecialChars(str) {
     return decodeURIComponent(str);
-}
-
-
-//아래 4개의 메소드는 액티비티 화면을 수정할때 사용됩니다.
-function onCreate(savedInstanceState, activity) {
+    }
+    
+    function onCreate(savedInstanceState, activity) {
     var textView = new android.widget.TextView(activity);
     textView.setText("Hello, World!");
     textView.setTextColor(android.graphics.Color.DKGRAY);
     activity.setContentView(textView);
-}
+    }
+    
+    function onStart(activity) { }
+    
+    function onResume(activity) { }
+    
+    function onPause(activity) { }
+    
+    function onStop(activity) { }
 
-
-
-function onStart(activity) { }
-
-function onResume(activity) { }
-
-function onPause(activity) { }
-
-function onStop(activity) { }
-
-function onNotificationPosted(sbn, sm) {
-    function replyKakaotalk(packageName, responseFix) {
-        var actions = sbn.getNotification().actions;
-        if (actions == null) return;
-        var userId = sbn.getUser().hashCode();
-        //Log.debug("userId : " + userId);
-        //log actions length
-        //Log.debug("actions length : " + actions.length);
-        for (var n = 0; n < actions.length; n++) {
-            var action = actions[n];
-            if (action.getRemoteInputs() == null) continue;
-            var bundle = sbn.getNotification().extras;
-            var msg = bundle.get("android.text").toString();
-            var sender = bundle.getString("android.title");
-            var room = bundle.getString("android.subText");
-            if (room == null) room = bundle.getString("android.summaryText");
-            var isGroupChat = room != null;
-            if (room == null) room = sender;
-            var replier = new com.xfl.msgbot.script.api.legacy.SessionCacheReplier(packageName, action, room, false, "");
-            var icon = bundle.getParcelableArray("android.messages")[0].get("sender_person").getIcon().getBitmap();
-            var image = bundle.getBundle("android.wearable.EXTENSIONS");
-            if (image != null) image = image.getParcelable("background");
-            var imageDB = new com.xfl.msgbot.script.api.legacy.ImageDB(icon, image);
-            com.xfl.msgbot.application.service.NotificationListener.Companion.setSession(packageName, room, action);
-            if (this.hasOwnProperty("responseFix")) {
-                responseFix(room, msg, sender, isGroupChat, replier, imageDB, packageName, userId != 0);
+    function onNotificationPosted(sbn, sm) {
+        function replyKakaotalk(packageName, responseFix) {
+            var actions = sbn.getNotification().actions;
+            if (actions == null) return;
+            var userId = sbn.getUser().hashCode();
+            //Log.debug("userId : " + userId);
+            //log actions length
+            //Log.debug("actions length : " + actions.length);
+            for (var n = 0; n < actions.length; n++) {
+                var action = actions[n];
+                if (action.getRemoteInputs() == null) continue;
+                var bundle = sbn.getNotification().extras;
+                var msg = bundle.get("android.text").toString();
+                var sender = bundle.getString("android.title");
+                var room = bundle.getString("android.subText");
+                if (room == null) room = bundle.getString("android.summaryText");
+                var isGroupChat = room != null;
+                if (room == null) room = sender;
+                var replier = new com.xfl.msgbot.script.api.legacy.SessionCacheReplier(packageName, action, room, false, "");
+                var icon = bundle.getParcelableArray("android.messages")[0].get("sender_person").getIcon().getBitmap();
+                var image = bundle.getBundle("android.wearable.EXTENSIONS");
+                if (image != null) image = image.getParcelable("background");
+                var imageDB = new com.xfl.msgbot.script.api.legacy.ImageDB(icon, image);
+                com.xfl.msgbot.application.service.NotificationListener.Companion.setSession(packageName, room, action);
+                if (this.hasOwnProperty("responseFix")) {
+                    responseFix(room, msg, sender, isGroupChat, replier, imageDB, packageName, userId != 0);
+                }
             }
         }
-    }
-    function replyMMS(textDataLog) {
-        Api.replyRoom("윤피티_문자메세지", textDataLog, false);
-    }
-    function replyETC(textDataLog) {
-        Api.replyRoom("윤피티_Etc", textDataLog, false);
-    }
-    function createTextDataLog(textData, packageName) {
-        return "Notification Text Data:\n" +
-            "  Title: " + textData.title + "\n" +
-            "  Text: " + textData.text + "\n" +
-            "  SubText: " + textData.subText + "\n" +
-            "  SummaryText: " + textData.summaryText + "\n" +
-            "  BigText: " + textData.bigText + "\n" +
-            "  InfoText: " + textData.infoText + "\n" +
-            "Package: " + packageName;
-    }
-
-    try {
-
-        // StatusBarNotification 객체에서 Notification 객체를 가져옵니다.
-        const notification = sbn.getNotification();
-
-        // Notification 객체에서 텍스트 관련 정보를 추출하여 객체에 저장합니다.
-        const textData = {
-            title: notification.extras.getCharSequence("android.title"),
-            text: notification.extras.getCharSequence("android.text"),
-            subText: notification.extras.getCharSequence("android.subText"),
-            summaryText: notification.extras.getCharSequence("android.summaryText"),
-            bigText: notification.extras.getCharSequence("android.bigText"),
-            infoText: notification.extras.getCharSequence("android.infoText")
-        };
-
-        // StatusBarNotification 객체에서 패키지 정보를 추출합니다.
-        const packageName = sbn.getPackageName();
-
-        // 텍스트 관련 정보와 패키지 정보를 Log.debug에 출력합니다.
-        const textDataLog = createTextDataLog(textData, packageName);
-
-        if (packageName.startsWith("com.samsung.android.messaging")) {
-            replyMMS(textDataLog)
+        function replyMMS(textDataLog) {
+            Api.replyRoom("윤피티_문자메세지", textDataLog, false);
+            }
+            function replyETC(textDataLog) {
+            Api.replyRoom("윤피티_Etc", textDataLog, false);
+            }
+            function createTextDataLog(textData, packageName) {
+                return "Notification Text Data:\n" +
+                "  Title: " + textData.title + "\n" +
+                "  Text: " + textData.text + "\n" +
+                "  SubText: " + textData.subText + "\n" +
+                "  SummaryText: " + textData.summaryText + "\n" +
+                "  BigText: " + textData.bigText + "\n" +
+                "  InfoText: " + textData.infoText + "\n" +
+                "Package: " + packageName;
         }
-        else if (packageName.startsWith("com.kakao.talk")) {
-            replyKakaotalk(packageName, responseFix)
+        try {
+            // StatusBarNotification 객체에서 Notification 객체를 가져옵니다.
+            const notification = sbn.getNotification();
+        
+            // Notification 객체에서 텍스트 관련 정보를 추출하여 객체에 저장합니다.
+            const textData = {
+                // ... 코드 생략 ...
+            };
+        
+            // StatusBarNotification 객체에서 패키지 정보를 추출합니다.
+            const packageName = sbn.getPackageName();
+        
+            // 텍스트 관련 정보와 패키지 정보를 Log.debug에 출력합니다.
+            const textDataLog = createTextDataLog(textData, packageName);
+        
+            if (packageName.startsWith("com.samsung.android.messaging")) {
+                replyMMS(textDataLog);
+            } else if (packageName.startsWith("com.kakao.talk")) {
+                replyKakaotalk(packageName, responseFix);
+            } else {
+                replyETC(textDataLog);
+            }
+        } catch (error) {
+            Log.debug("Error in onNotificationPosted: " + error.message);
         }
-        else {
-            replyETC(textDataLog)
-
-        }
-    } catch (error) {
-        Log.debug("Error in onNotificationPosted: " + error.message);
     }
-
-
-
-}
-
